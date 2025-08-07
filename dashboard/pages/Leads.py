8f933d7f8f02079772505435a0164855ed2652f3
Prati-Emp <pratiprojetos@gmail.com>
2025-08-07 16:14:01 -0300

    Update Leads.py

---
+++ dashboard/pages/Leads.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Em Desenvolvimento", page_icon="🚧")
+
+st.title("🚧 Página em Desenvolvimento")
+st.write("Esta página ainda está sendo construída. Por favor, volte em breve.")
