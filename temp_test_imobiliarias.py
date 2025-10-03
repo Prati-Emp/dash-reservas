eb079e8563688eb419c9d613f4bc2a072f71d346
Prati-Emp <pratiprojetos@gmail.com>
2025-10-03 14:30:03 -0300

    Update temp_test_imobiliarias.py

--- temp_test_imobiliarias.py
+++ temp_test_imobiliarias.py
@@ -1,9 +1,6 @@
 import duckdb
 import pandas as pd
 
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InByYXRpcHJvamV0b3NAZ21haWwuY29tIiwic2Vzc2lvbiI6InByYXRpcHJvamV0b3MuZ21haWwuY29tIiwicGF0IjoiUnA1clVla2JwRFY4OFp2d3RKNWxkOFhxdmtpSFQzRlNacWdXbXFsQ09WMCIsInVzZXJJZCI6ImFkZThmZGM0LTc1MDktNGU4Ny1hZTcwLTMwZGVkMTQ4Y2RlOSIsImlzcyI6Im1kX3BhdCIsInJlYWRPbmx5IjpmYWxzZSwidG9rZW5UeXBlIjoicmVhZF93cml0ZSIsImlhdCI6MTc0OTA2ODI4N30.TEUsvAxCKXhzNrb7WAok0jL2YmqEEtrxaEOKZZ6tuBI"
-
-con = duckdb.connect(f"md:reservas?token={token}")
 
 # Unique imobiliarias
 df = con.execute("SELECT DISTINCT Imobiliaria FROM cv_leads").df()
